1fc16729f196af7c44143027cdceecd2a8cdb413
Cody Wellman <zagdrath@member.fsf.org>
2022-03-22 08:33:47 -0400

    pixy test stuff

--- src/main/java/com/team3602/robot/subsystems/PixySubsystem.java
+++ src/main/java/com/team3602/robot/subsystems/PixySubsystem.java
@@ -4,11 +4,17 @@
 
 package com.team3602.robot.subsystems;
 
+// Java imports
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // WPILib imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // Pixy2JavaAPI imports
 import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
 
 /**
@@ -22,14 +28,45 @@ import io.github.pseudoresonance.pixy2api.links.SPILink;
 public class PixySubsystem extends SubsystemBase {
   private static final Pixy2 pixyCamera = Pixy2.createInstance(new SPILink());
 
+  private static Block blockColor = null;
+
   public PixySubsystem() {
     pixyCamera.init();
     pixyCamera.setLamp((byte) 1, (byte) 1);
     pixyCamera.setLED(255, 255, 255);
+
+    getBiggestBlock();
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public static Block getBiggestBlock() {
+    // Gets the number of "blocks", identified targets, that match signature 1 on
+    // the Pixy2, does not wait for new data if none is available, and limits the
+    // number of returned blocks to 25, for a slight increase in efficiency
+    int blockCount = pixyCamera.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+
+    System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
+
+    if (blockCount <= 0) {
+      System.err.println("No blocks found!");
+      return null; // If blocks were not found, stop processing
+    }
+
+    ArrayList<Block> blocks = pixyCamera.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
+
+    // for (Block block : blocks) { // Loops through all blocks and finds the widest one
+    //   if (blockColor == null) {
+    //     blockColor = block;
+    //   } else if (block.getSignature() > blockColor.getSignature()) {
+    //     blockColor = block;
+    //   }
+    // }
+    return blockColor;
   }
+
+  public void logDataToSmartDashboard() {
+    SmartDashboard.putNumber("Block X", blockColor.getSignature());
+    SmartDashboard.putNumber("Block X", blockColor.getX());
+    SmartDashboard.putNumber("Block Y", blockColor.getY());
+  }
+
 }
